60c5702e1e4f9f89fb777d201b58cf46c30a0913
karthikeya12082005 <karthikeya.kasam@gmail.com>
2025-08-11 10:47:14 +0530

    Update first.py

--- week-3/first.py
+++ week-3/first.py
@@ -1,1 +1,7 @@
+import os
+pid = os.fork()
+if pid == 0:
+    print(f"Child Process: PID={os.getpid()}, Parent PID={os.getppid()}")
+else:
+    print(f"Parent Process: PID={os.getpid()}, Child PID={pid}")
 
